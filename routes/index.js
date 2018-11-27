62781db1d8e4e8cbbd4ad8c0821e1d2d53acad75
EC2 Default User <ec2-user@ip-172-31-7-186.us-east-2.compute.internal>
2018-11-27 20:52:40 +0000

    angular problem

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,35 @@
 var express = require('express');
-var fs = require('fs');
-var request = require('request');
 var router = express.Router();
 
+var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
+
+mongoose.connect('mongodb://localhost/commentDB',{ useNewUrlParser: true }); //Connects to a mongo database called "commentDB"
+
+var commentSchema = mongoose.Schema({ //Defines the Schema for this database
+    Name: String,
+    Comment: String
+});
+
+var Comment = mongoose.model('Comment', commentSchema); //Makes an object from that schema as a model
+
+var db = mongoose.connection; //Saves the connection as a variable to use
+db.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
+db.once('open', function() { //Lets us know when we're connected
+    console.log('Connected');
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.sendFile('weather.html', { root: 'public' });
+  res.render('index', { title: 'Express' });
 });
 
-router.get('/getcity', function(req, res, next) {
-
-    fs.readFile(__dirname + '/cities.dat.txt', function(err, data) {
-        if (err) throw err;
-        var cities = data.toString().split("\n");
-
-        var myRe = new RegExp("^" + req.query.q);
-
-        for (var i = 0; i < cities.length; i++) {
-            var result = cities[i].search(myRe);
-        }
-
-        var jsonresult = [];
-        for (var i = 0; i < cities.length; i++) {
-            var result = cities[i].search(myRe);
-            if (result != -1) {
-                jsonresult.push({ city: cities[i] });
-            }
-        }
-        
-        
-        res.status(200).json(jsonresult);
-
-    });
 
 
-});
-
-router.get('/dictionary', function(req, res, next) {
+router.post('/comment', function(req, res, next) {
     
-  var url = "https://owlbot.info/api/v1/dictionary/";
-  
-  url += req.query['q'];
-  
-  url += "?format=json";
-  
-  console.log(url);
-  request(url).pipe(res);
+console.log("POST comment route"); 
+console.log(req.body); 
+res.sendStatus(200);
 });
-
-
-
-
 
 module.exports = router;
